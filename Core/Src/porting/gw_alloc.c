0cde4b29411ee8e3d4349f0808d708de9735726f
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-12-14 22:22:47 +0100

    Fix heap usage

--- Core/Src/porting/gw_alloc.c
+++ Core/Src/porting/gw_alloc.c
@@ -43,9 +43,20 @@ void *rg_alloc(size_t size, uint32_t caps)
     return &p[1];
 }
 
-void rg_free(void *_p)
+void *rg_calloc(size_t nmemb, size_t size)
 {
-    uint32_t *p = ((uint32_t *) _p) - 1;
+    uint8_t *p = rg_alloc(nmemb * size, 0);
+
+    memset(p, '\x00', nmemb * size);
+
+    return p;
+}
+
+void rg_free(void *ptr)
+{
+    assert(ptr != NULL);
+
+    uint32_t *p = ((uint32_t *) ptr) - 1;
 
     _total_alloc_bytes -= p[0];
     _total_alloc_bytes_actual -= p[0] + sizeof(uint32_t);
@@ -53,6 +64,27 @@ void rg_free(void *_p)
     printf("F %d %d %d %p\n", p[0], _total_alloc_bytes, _total_alloc_bytes_actual, p);
 
     free(p);
+}
+
+void *rg_realloc(void *ptr, size_t size)
+{
+    if (ptr == NULL) {
+        return rg_alloc(size, 0);
+    }
+
+    uint32_t *p = ((uint32_t *) ptr) - 1;
+
+    size_t old_size = p[0];
+
+    _total_alloc_bytes -= p[0];
+    _total_alloc_bytes_actual -= p[0] + sizeof(uint32_t);
+
+    _total_alloc_bytes += size;
+    _total_alloc_bytes_actual += size + sizeof(uint32_t);
+
+    printf("R %d=>%d %d %d %p\n", old_size, size, _total_alloc_bytes, _total_alloc_bytes_actual, p);
+
+    return realloc(p, size);
 }
 
 #endif
