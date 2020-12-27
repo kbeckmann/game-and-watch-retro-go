84e74f5b1c193ed102f24355fc67117cfe7fb38f
Franz Levin <frazz@kryo.se>
2020-12-28 00:41:39 +0100

    cleanup

--- Core/Src/porting/gw_alloc.c
+++ Core/Src/porting/gw_alloc.c
@@ -7,8 +7,7 @@
 
 
 void *
-_sbrk (incr)
-     int incr;
+_sbrk (int incr)
 {
     static char * heap_end;
     char *        prev_heap_end;
@@ -26,19 +25,21 @@ _sbrk (incr)
 
 #ifdef DEBUG_RG_ALLOC
 
-size_t   _total_alloc_bytes;
-size_t   _total_alloc_bytes_actual;
-uint32_t _total_alloc_num;
+static struct {
+    size_t   total_alloc_bytes;
+    size_t   total_alloc_bytes_actual;
+    uint32_t total_alloc_num;
+} alloc_data;
 
 void *rg_alloc(size_t size, uint32_t caps)
 {
     uint32_t *p = malloc(size + sizeof(uint32_t));
 
-    _total_alloc_bytes += size;
-    _total_alloc_bytes_actual += size + sizeof(uint32_t);
+    alloc_data.total_alloc_bytes += size;
+    alloc_data.total_alloc_bytes_actual += size + sizeof(uint32_t);
 
 #ifdef DEBUG_RG_ALLOC_PRINT
-    printf("A %d %d %d %p\n", size, _total_alloc_bytes, _total_alloc_bytes_actual, p);
+    printf("A %d %d %d %p\n", size, alloc_data.total_alloc_bytes, alloc_data.total_alloc_bytes_actual, p);
 #endif
 
     p[0] = size;
@@ -61,11 +62,15 @@ void rg_free(void *ptr)
 
     uint32_t *p = ((uint32_t *) ptr) - 1;
 
-    _total_alloc_bytes -= p[0];
-    _total_alloc_bytes_actual -= p[0] + sizeof(uint32_t);
+    alloc_data.total_alloc_bytes -= p[0];
+    alloc_data.total_alloc_bytes_actual -= p[0] + sizeof(uint32_t);
 
 #ifdef DEBUG_RG_ALLOC_PRINT
-    printf("F %d %d %d %p\n", p[0], _total_alloc_bytes, _total_alloc_bytes_actual, p);
+    printf("F %d %d %d %p\n",
+            p[0],
+            alloc_data.total_alloc_bytes,
+            alloc_data.total_alloc_bytes_actual,
+            p);
 #endif
 
     free(p);
@@ -79,14 +84,19 @@ void *rg_realloc(void *ptr, size_t size)
 
     uint32_t *p = ((uint32_t *) ptr) - 1;
 
-    _total_alloc_bytes -= p[0];
-    _total_alloc_bytes_actual -= p[0] + sizeof(uint32_t);
+    alloc_data.total_alloc_bytes -= p[0];
+    alloc_data.total_alloc_bytes_actual -= p[0] + sizeof(uint32_t);
 
-    _total_alloc_bytes += size;
-    _total_alloc_bytes_actual += size + sizeof(uint32_t);
+    alloc_data.total_alloc_bytes += size;
+    alloc_data.total_alloc_bytes_actual += size + sizeof(uint32_t);
 
 #ifdef DEBUG_RG_ALLOC_PRINT
-    printf("R %d=>%d %d %d %p\n", old_size, size, _total_alloc_bytes, _total_alloc_bytes_actual, p);
+    printf("R %d=>%d %d %d %p\n",
+            old_size,
+            size,
+            alloc_data.total_alloc_bytes,
+            alloc_data.total_alloc_bytes_actual,
+            p);
 #endif
 
     return realloc(p, size);
