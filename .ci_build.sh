399143906f85f79db13722ed0d0237a0a58abadc
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-12-27 16:46:31 +0100

    Fix CI

--- .ci_build.sh
+++ .ci_build.sh
@@ -7,14 +7,11 @@ sudo apt-get install -y gcc-arm-none-eabi binutils-arm-none-eabi
 git submodule update --init --recursive
 
 # NES
-curl -L -o pwn.bin https://github.com/Vector35/PwnAdventureZ/blob/master/PwnAdventureZ-csaw-withkeys.bin?raw=true
-./update_nes_rom.sh pwn.bin
-make -f Makefile.nes -j clean
-make -f Makefile.nes -j
-
+curl -L -o roms/nes/pwn.bin https://github.com/Vector35/PwnAdventureZ/blob/master/PwnAdventureZ-csaw-withkeys.bin?raw=true
 
 # GB
-curl -L -o matrix-rain.gb https://github.com/wtjones/matrix-rain-gb/releases/download/0.0.3/matrix-rain.gb
-./update_gb_rom.sh matrix-rain.gb
-make -f Makefile.gb -j clean
+curl -L -o roms/gb/matrix-rain.gb https://github.com/wtjones/matrix-rain-gb/releases/download/0.0.3/matrix-rain.gb
+
+python3 ./parse_roms.py
+
 make -f Makefile.gb -j
