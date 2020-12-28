67de08a97c3ecea453b18314dc0734c738f75e7f
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-12-28 18:31:59 +0100

    Use ubuntubuilder-arm

--- .ci_build.sh
+++ .ci_build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -xe
 
-sudo apt-get install -y gcc-arm-none-eabi binutils-arm-none-eabi
-
 # Would be nice to clone the repo with submodules. If you know how to do this, please make a PR!
 git submodule update --init --recursive
 
