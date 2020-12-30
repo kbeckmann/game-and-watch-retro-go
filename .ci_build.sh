1737b1c35afdb06ae0ce7812d7659aec8927a6c9
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-12-30 16:15:45 +0100

    ci: Fix filename for NES rom

--- .ci_build.sh
+++ .ci_build.sh
@@ -5,7 +5,7 @@ set -xe
 git submodule update --init --recursive
 
 # NES
-curl -L -o roms/nes/pwn.bin https://github.com/Vector35/PwnAdventureZ/blob/master/PwnAdventureZ-csaw-withkeys.bin?raw=true
+curl -L -o roms/nes/pwn.nes https://github.com/Vector35/PwnAdventureZ/blob/master/PwnAdventureZ-csaw-withkeys.bin?raw=true
 
 # GB
 curl -L -o roms/gb/matrix-rain.gb https://github.com/wtjones/matrix-rain-gb/releases/download/0.0.3/matrix-rain.gb
