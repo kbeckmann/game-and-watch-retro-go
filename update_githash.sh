9830937d105c8dd1ae389b687568d2c61c46b0f4
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-01-04 02:53:12 +0100

    Simplify handling of updated files.

---
+++ update_githash.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+if [ -z $1 ]; then
+    echo "Usage: `basename $0` <githash.h>"
+    exit 1
+fi
+
+githashfile="$1"
+
+TMPFILE=$(mktemp build/githash.XXXXXX)
+if [[ ! -e $TMPFILE ]]; then
+    echo "Can't create tempfile!"
+    exit 1
+fi
+
+GITHASH=$(git describe --always --dirty 2> /dev/null || echo "NOGIT")
+
+echo -e "#ifndef GIT_HASH\n#define GIT_HASH \""${GITHASH}\""\n#endif" > "${TMPFILE}"
+
+if ! diff -q ${TMPFILE} ${githashfile} > /dev/null 2> /dev/null; then
+    echo "Updating git hash file ${githashfile}"
+    cp -f "${TMPFILE}" "${githashfile}"
+fi
+
+rm -f "${TMPFILE}"
---
+++ update_rom_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+if [ -z $2 ]; then
+	echo "Usage: `basename $0` <file list> <rom directory> [<rom directory>] ..."
+	exit 1
+fi
+
+filelist="$1"
+
+TMPFILE=$(mktemp build/hashes.XXXXXX)
+if [[ ! -e $TMPFILE ]]; then
+	echo "Can't create tempfile!"
+	exit 1
+fi
+
+find ${@:2} -type f > "${TMPFILE}" 2> /dev/null
+
+if ! diff -q ${TMPFILE} ${filelist} > /dev/null 2> /dev/null; then
+    echo "Updating file list ${filelist}"
+	cp -f "${TMPFILE}" "${filelist}"
+fi
+
+rm -f "${TMPFILE}"
