461d43380f4684c878d70a76c722a376439e5655
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-02-18 08:21:49 +0100

    ci: Download a PCE rom

--- .ci_build.sh
+++ .ci_build.sh
@@ -14,7 +14,11 @@ curl -L -o roms/gb/matrix-rain.gb https://github.com/wtjones/matrix-rain-gb/rele
 # Download and install GG and SMS open source demoscene ROM
 curl -L -o sega_tween.zip https://files.scene.org/get/demos/artists/ben_ryves/sega_tween.zip
 unzip sega_tween.zip
-cp 'Sega Tween (Normal).gg' roms/gg
-cp 'Sega Tween (Normal).sms' roms/sms
+
+
+# Download and install a PCE democene ROM
+curl -L -o UP-19PCE.zip http://blockos.org/releases/demos/pce/UP-19PCE.zip
+unzip UP-19PCE.zip
+mv UP-19PCE.pce ./roms/pce/
 
 make -j $(nproc)
