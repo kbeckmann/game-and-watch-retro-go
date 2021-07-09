ff034689bbb01d938c5d6e018583db08aa81c943
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-07-10 01:44:24 +0200

    ci: Don't download toolchains from github
    Github has quotas on LFS which make this problematic.

--- .ci_build_macos.sh
+++ .ci_build_macos.sh
@@ -7,9 +7,7 @@ git submodule update --init --recursive
 ./.ci_prepare_roms.sh
 
 # Install toolchain
-# curl -L -o toolchain.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2?revision=48a4e09a-eb5a-4eb8-8b11-d65d7e6370ff&la=en&hash=8AACA5F787C5360D2C3C50647C52D44BCDA1F73F
-# Downloading from developer.arm.com is slow, use github mirror instead
-curl -L -o toolchain.tar.bz2 https://github.com/kbeckmann/gcc-toolchains/raw/main/10-2020-q4-major/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2
+curl -L -o toolchain.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2?revision=48a4e09a-eb5a-4eb8-8b11-d65d7e6370ff&la=en&hash=8AACA5F787C5360D2C3C50647C52D44BCDA1F73F
 
 tar xf toolchain.tar.bz2
 
