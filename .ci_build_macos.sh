0850193c9a7cb0556abe1ff88806014dcd65c571
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-07-10 11:42:00 +0200

    ci: Download toolchain from a mirror
    Also fixes submodule config for the ci jobs

--- .ci_build_macos.sh
+++ .ci_build_macos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -xe
 
-# Would be nice to clone the repo with submodules. If you know how to do this, please make a PR!
-git submodule update --init --recursive
-
 ./.ci_prepare_roms.sh
 
 # Install toolchain
-curl -L -o toolchain.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2?revision=48a4e09a-eb5a-4eb8-8b11-d65d7e6370ff&la=en&hash=8AACA5F787C5360D2C3C50647C52D44BCDA1F73F
+# curl -L -o toolchain.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2?revision=48a4e09a-eb5a-4eb8-8b11-d65d7e6370ff&la=en&hash=8AACA5F787C5360D2C3C50647C52D44BCDA1F73F
+
+# Faster mirror:
+curl -L --http1.1 -o toolchain.tar.bz2 https://allg.one/7vMO
 
 tar xf toolchain.tar.bz2
 
--- .ci_build_ubuntu.sh
+++ .ci_build_ubuntu.sh
@@ -3,9 +3,6 @@ set -xe
 
 apt install lz4
 
-# Would be nice to clone the repo with submodules. If you know how to do this, please make a PR!
-git submodule update --init --recursive
-
 ./.ci_prepare_roms.sh
 
 make -j $(nproc)
