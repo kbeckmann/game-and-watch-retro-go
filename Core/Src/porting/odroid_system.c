23dd2dc84def5c96892e8ac2fc61ce9eeae482c4
Rustam Zinurov <rzinurov@gmail.com>
2021-07-14 22:15:01 +0200

    Remember the last selected game in the menu on power off

--- Core/Src/porting/odroid_system.c
+++ Core/Src/porting/odroid_system.c
@@ -118,10 +118,9 @@ runtime_stats_t odroid_system_get_stats()
 void odroid_system_sleep(void)
 {
     odroid_settings_StartupFile_set(ACTIVE_FILE);
-    odroid_settings_MainMenuSelectedTab_set(0);
-    odroid_settings_MainMenuCursor_set(0);
 
-    odroid_settings_commit();
+    // odroid_settings_commit();
+    gui_save_current_tab();
 
     GW_EnterDeepSleep();
 }
