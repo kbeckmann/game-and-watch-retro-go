b5d19593cf4e5a5a670c5227d889714bd0836c52
Brian Pugh <bnp117@gmail.com>
2021-08-04 09:56:53 -0700

    Fix sms from emulating 5% too fast introduced in b6bbe8e2. Slightly increase target frame period accuracy for all emulators

--- Core/Src/porting/common.c
+++ Core/Src/porting/common.c
@@ -71,7 +71,7 @@ void odroid_audio_mute(bool mute)
 }
 
 common_emu_state_t common_emu_state = {
-    .frame_time_10us = 100000 / 60,  // Most (all?) emus are 60fps
+    .frame_time_10us = (uint16_t)(100000 / 60 + 0.5f),  // Reasonable default of 60FPS if not explicitly configured.
 };
 
 
