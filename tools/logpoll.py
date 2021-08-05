72da767238981b8a15fb76d9bf0c36fedab1c9db
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-08-05 21:57:04 +0200

    tools: Break out openocd and rewrite screengrabber a bit

--- tools/logpoll.py
+++ tools/logpoll.py
@@ -1,10 +1,11 @@
-# /bin/env python3
+#!/usr/bin/env python3
 
 from elftools.elf.elffile import ELFFile
 
+from openocd import OpenOCD
+
 import argparse
 import time
-import socket
 import sys
 
 def get_symbol_by_symbol_name(elffile, symbol_name):
@@ -14,55 +15,6 @@ def strtohex(data):
     return map(strtohex, data) if isinstance(data, list) else int(data, 16)
 
 # OpenOCD class cherry-picked/inspired from from https://github.com/zmarvel/python-openocd
-
-class OpenOCD():
-    COMMAND_TOKEN = '\x1a'
-
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.buffer_size = 4096
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def __enter__(self):
-        self.sock.connect((self.host, self.port))
-        return self
-
-    def __exit__(self, type, value, traceback):
-        try:
-            self.send("exit")
-        finally:
-            self.sock.close()
-
-    def send(self, cmd):
-        """Send a command string to TCL RPC. Return the result that was read."""
-        data = (cmd + OpenOCD.COMMAND_TOKEN).encode("utf-8")
-        self.sock.send(data)
-        return self._recv()
-
-    def _recv(self):
-        """Read from the stream until the token (\x1a) was received."""
-        data = bytes()
-        while True:
-            chunk = self.sock.recv(self.buffer_size)
-            data += chunk
-            if bytes(OpenOCD.COMMAND_TOKEN, encoding="utf-8") in chunk:
-                break
-
-        data = data.decode("utf-8").strip()
-        data = data[:-1]  # strip trailing \x1a
-
-        return data
-
-    def read_memory(self, wordLen, address, n):
-        self.send("array unset output")  # better to clear the array before
-        self.send("mem2array output %d 0x%x %d" % (wordLen, address, n))
-
-        output = self.send("capture \"echo \$output\"").split(" ")
-
-        return [int(output[2*i+1]) for i in range(len(output)//2)]
-
 
 def logpoll(args):
     with OpenOCD(host=args.host, port=args.port) as ocd:
