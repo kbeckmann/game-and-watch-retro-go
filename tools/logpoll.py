9d56ddc0bce346629fb7293f0e4fbaff83df8939
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-08-05 21:14:00 +0200

    tools: Fix wrapping bug and add halt support for safer data reads

--- tools/logpoll.py
+++ tools/logpoll.py
@@ -1,14 +1,14 @@
 # /bin/env python3
 
 from elftools.elf.elffile import ELFFile
-from time import sleep
 
 import argparse
+import time
 import socket
 import sys
 
-def get_address_by_symbol_name(elffile, symbol_name):
-    return elffile.get_section_by_name('.symtab').get_symbol_by_name(symbol_name)[0].entry.st_value
+def get_symbol_by_symbol_name(elffile, symbol_name):
+    return elffile.get_section_by_name('.symtab').get_symbol_by_name(symbol_name)[0]
 
 def strtohex(data):
     return map(strtohex, data) if isinstance(data, list) else int(data, 16)
@@ -70,25 +70,43 @@ def logpoll(args):
 
         with open(args.elf, "rb") as f:
             elffile = ELFFile(f)
-            logbuf_addr = get_address_by_symbol_name(elffile, "logbuf")
-            log_idx_addr = get_address_by_symbol_name(elffile, "log_idx")
+            logbuf = get_symbol_by_symbol_name(elffile, "logbuf")
+            logbuf_addr = logbuf.entry.st_value
+            logbuf_size = logbuf.entry.st_size
+            log_idx_addr = get_symbol_by_symbol_name(elffile, "log_idx").entry.st_value
 
         ocd.send("resume")
 
         while True:
+            if args.halt:
+                ocd.send("halt")
+
             log_idx = ocd.read_memory(32, log_idx_addr, 1)[0]
+
             if log_idx > last_idx:
                 # print the new data since last iteration
                 logbuf = ocd.read_memory(8, logbuf_addr + last_idx, log_idx - last_idx)
                 logbuf_str = "".join([chr(c) for c in logbuf])
                 sys.stdout.write(logbuf_str)
             elif log_idx > 0 and log_idx < last_idx:
-                # print the new data starting from 0
+                # Get new data from the end of the buffer until the first null byte
+                logbuf = ocd.read_memory(8, logbuf_addr + last_idx, logbuf_size - last_idx)
+                if 0 in logbuf:
+                    end = logbuf.index(0) - 1
+                else:
+                    end = len(logbuf)
+                logbuf_str = "".join([chr(c) for c in logbuf[:end]])
+
+                # Read new data from the beginning and append it
                 logbuf = ocd.read_memory(8, logbuf_addr, log_idx)
-                logbuf_str = "".join([chr(c) for c in logbuf])
+                logbuf_str += "".join([chr(c) for c in logbuf])
                 sys.stdout.write(logbuf_str)
+
+            if args.halt:
+                ocd.send("resume")
+
             last_idx = log_idx
-            sleep(args.interval / 1000)
+            time.sleep(args.interval / 1000)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Polls the stdout log from a running target")
@@ -116,6 +134,12 @@ if __name__ == "__main__":
         type=int,
         default=6666,
         help="OpenOCD TCL port",
+    )
+    parser.add_argument(
+        "--halt",
+        dest="halt",
+        action="store_true",
+        help="Halts the target during memory reads",
     )
 
     logpoll(parser.parse_args())
