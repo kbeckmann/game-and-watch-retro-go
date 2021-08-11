2384a731b87357d0f020c4e15b6d46c752f0c40a
northskysl <northsky@sudenluola.org>
2021-08-11 23:01:27 +0300

    Update rg_rtc.c
    Add a nicer year display

--- Core/Src/retro-go/rg_rtc.c
+++ Core/Src/retro-go/rg_rtc.c
@@ -343,7 +343,7 @@ bool date_display_cb(odroid_dialog_choice_t *option, odroid_dialog_event_t event
     HAL_RTC_GetTime(&hrtc, &GW_currentTime, RTC_FORMAT_BIN);
     HAL_RTC_GetDate(&hrtc, &GW_currentDate, RTC_FORMAT_BIN);
     
-    sprintf(option->value, "%02d.%02d.%02d %s", GW_currentDate.Date, GW_currentDate.Month, GW_currentDate.Year, (char *) GW_RTC_Weekday[GW_currentDate.WeekDay-1]);
+    sprintf(option->value, "%02d.%02d.20%02d %s", GW_currentDate.Date, GW_currentDate.Month, GW_currentDate.Year, (char *) GW_RTC_Weekday[GW_currentDate.WeekDay-1]);
     return event == ODROID_DIALOG_ENTER;
     return false;
 }
