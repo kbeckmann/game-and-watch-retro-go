7ba5232828acabf4fb1b4e1f7e616a08c45562e9
bzhxx <77247723+bzhxx@users.noreply.github.com>
2021-08-16 21:12:04 +0200

    add gw to saves restore script (#135)

--- scripts/saves_restore.sh
+++ scripts/saves_restore.sh
@@ -19,7 +19,7 @@ fi
 
 mkdir -p "$INDIR"
 
-for emu in gb gg nes pce sms; do
+for emu in gb gg gw nes pce sms; do
     mkdir -p "${INDIR}/${emu}"
     COUNT=$(get_number_of_saves SAVE_$(echo ${emu} | awk '{print toupper($0)}')_)
     for (( i = 0; i < COUNT; i++ )); do
