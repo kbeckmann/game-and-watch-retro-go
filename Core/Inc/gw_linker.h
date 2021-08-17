278616969e853b56ba3088b800645246a93f0911
Brian Pugh <bnp117@gmail.com>
2021-08-17 11:13:39 -0700

    fix savestates from saving/loading from the wrong location when __EXFLASH_OFFSET__ is specified (#136)

--- Core/Inc/gw_linker.h
+++ Core/Inc/gw_linker.h
@@ -5,6 +5,7 @@
 extern uint8_t __SAVEFLASH_START__;
 extern uint8_t __SAVEFLASH_END__;
 extern uint8_t __EXTFLASH_START__;
+extern uint8_t __EXTFLASH_BASE__;
 extern uint32_t __INTFLASH__;  // From linker, usually value 0x08000000 for bank 1, or 0x08100000 for bank 2
 
 extern uint8_t __NULLPTR_LENGTH__;
