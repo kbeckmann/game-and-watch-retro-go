2a9bf6d719a60ea94fafc7254567191caf80e99a
Konrad Beckmann <konrad.beckmann@gmail.com>
2021-11-06 13:09:49 +0100

    ci: Remove --http1.1 flag

--- scripts/.ci_build_macos.sh
+++ scripts/.ci_build_macos.sh
@@ -7,7 +7,7 @@ set -xe
 # curl -L -o toolchain.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2?revision=48a4e09a-eb5a-4eb8-8b11-d65d7e6370ff&la=en&hash=8AACA5F787C5360D2C3C50647C52D44BCDA1F73F
 
 # Faster mirror:
-curl -L --http1.1 -o toolchain.tar.bz2 https://allg.one/7vMO
+curl -L -o toolchain.tar.bz2 https://allg.one/7vMO
 
 tar xf toolchain.tar.bz2
 
