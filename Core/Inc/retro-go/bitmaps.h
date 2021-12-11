78c0bcaea57a08492eb7cc0245e5f6646f3de3fa
olderzeus <olderzeus@gmail.com>
2021-12-11 18:13:13 +0800

    Move image store locations

--- Core/Inc/retro-go/bitmaps.h
+++ Core/Inc/retro-go/bitmaps.h
@@ -26,3 +26,21 @@ extern const retro_logo_image logo_coleco;
 extern const retro_logo_image logo_nitendo;
 extern const retro_logo_image logo_sega;
 extern const retro_logo_image logo_pce;
+
+
+extern const unsigned char IMG_SPEAKER[];
+extern const unsigned char IMG_SUN[];
+extern const unsigned char IMG_FOLDER[];
+extern const unsigned char IMG_DISKETTE[];
+extern const unsigned char IMG_0_5X[];
+extern const unsigned char IMG_0_75X[]; 
+extern const unsigned char IMG_1X[];
+extern const unsigned char IMG_1_25X[];
+extern const unsigned char IMG_1_5X[];
+extern const unsigned char IMG_2X[];
+extern const unsigned char IMG_3X[];
+extern const unsigned char IMG_SC[];
+extern const unsigned char IMG_BUTTON_A[];
+extern const unsigned char IMG_BUTTON_A_P[];
+extern const unsigned char IMG_BUTTON_B[];
+extern const unsigned char IMG_BUTTON_B_P[];
