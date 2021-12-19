b0a0559ffc8f6173c19f036d0c2f4720ed155205
olderzeus <olderzeus@gmail.com>
2021-12-19 09:27:24 +0800

    new art item

--- Core/Inc/retro-go/bitmaps.h
+++ Core/Inc/retro-go/bitmaps.h
@@ -22,6 +22,14 @@ extern const retro_logo_image header_nes;
 extern const retro_logo_image header_pce;
 extern const retro_logo_image header_sms;
 extern const retro_logo_image header_gw;
+extern const retro_logo_image pad_sg1000;
+extern const retro_logo_image pad_col;
+extern const retro_logo_image pad_gb;
+extern const retro_logo_image pad_gg;
+extern const retro_logo_image pad_nes;
+extern const retro_logo_image pad_pce;
+extern const retro_logo_image pad_sms;
+extern const retro_logo_image pad_gw;
 
 extern const retro_logo_image logo_coleco;
 extern const retro_logo_image logo_nitendo;
